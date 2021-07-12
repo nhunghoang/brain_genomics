c4b44bc49cc9c7ce5dbf05f0e78f53a2b435b7f8
Nhung Hoang <nhoang4673@gmail.com>
2021-07-12 09:23:46 -0500

    save copies of the hcp vcf containing only the 891 subjects of interest, and as all/pdx snps

--- analyses_HCP/heritability/write_vcf.py
+++ analyses_HCP/heritability/write_vcf.py
@@ -7,22 +7,28 @@ samples.
 
 import h5py 
 import numpy as np 
+from time import time 
 
+## pretty-print time
+def pretty_time(tpass):
+    hr = int(tpass//3600)
+    mn = int(tpass%3600//60)
+    sc = int(tpass%3600%60)
+    return '{:d} hr, {:d} mn, {:d} sc'.format(hr, mn, sc)
+
+## input paths 
 all_dir = '/data1/rubinov_lab/brain_genomics/data_HCP/Marchini_phg000989/snps_by_chrom_hg38' 
 pdx_dir = '/data1/rubinov_lab/brain_genomics/data_HCP/predixcan_v8/vcf_format' 
 
-samp_all_dir = '/data1/rubinov_lab/brain_genomics/analyses_HCP/heritability/vcf_hoacer/all'
-samp_pdx_dir = '/data1/rubinov_lab/brain_genomics/analyses_HCP/heritability/vcf_hoacer/pdx'
+## output paths 
+samp_all_dir = '/data1/rubinov_lab/brain_genomics/analyses_HCP/heritability/vcf/all'
+samp_pdx_dir = '/data1/rubinov_lab/brain_genomics/analyses_HCP/heritability/vcf/pdx'
 
-ts_file = '/data1/rubinov_lab/brain_genomics/data_hoacer/timeseries_order.hdf5'
-mp_file = '/data1/rubinov_lab/brain_genomics/analyses_HCP/multi_gene_assoc/ccc_constants.hdf5' 
-
-## gather sample IDs for which we have timeseries for (in timeseries order)  
-with h5py.File(mp_file, 'r') as f: IDs = np.array(f['IDs'], dtype=str)
-id_map = {IDs[1][s]:'MH0'+IDs[0][s] for s in range(IDs.shape[1])} ## k: subj ID, v: samp ID 
-
-with h5py.File(ts_file, 'r') as f: subjects = np.array(f['subjects'], dtype=str)
-samples = [id_map[s] for s in subjects]
+## gather corresponding IDs for analysis cohort (n = 891)  
+map_file = '/data1/rubinov_lab/brain_genomics/analyses_HCP/multi_gene_assoc/ccc_constants.hdf5' 
+with h5py.File(map_file, 'r') as f: IDs = np.array(f['IDs'], dtype=str)
+samples = ['MH0'+s for s in IDs[0]]
+print('{} samples'.format(len(samples)))
 
 ## prepare header (and note the sample columns to ignore) 
 with open(all_dir + '/chr22.vcf', 'r') as f:
@@ -37,37 +43,43 @@ samp_subset, all_idx, samp_idx = np.intersect1d(all_samples, samples, \
 
 new_cat_names = non_samp_cats + '\t'.join(samp_subset) + '\n' 
 
-## write new vcf files containing just the hoacer samples 
-for ch in range(21, 0, -1): 
-    print(ch) 
-
-    all_vcf = '{}/chr{}.vcf'.format(all_dir, ch)
-    with open(all_vcf, 'r') as f: all_lines = f.readlines() 
-
-    pdx_vcf = '{}/chr{}.vcf'.format(pdx_dir, ch)
-    with open(pdx_vcf, 'r') as f: pdx_lines = f.readlines() 
+## write new vcf files containing just the samples being used 
+for ch in range(22, 0, -1): 
+    tstart = time() 
 
     ## all snps 
+    all_vcf = '{}/chr{}.vcf'.format(all_dir, ch)
+    with open(all_vcf, 'r') as f: all_lines = f.readlines() 
+    print('[{:>2}] read ALL variants'.format(ch))
+
     f_samp_all = '{}/chr{}.vcf'.format(samp_all_dir, ch) 
     with open(f_samp_all, 'w') as f: 
         f.write(header)
         f.write(new_cat_names) 
         for line in all_lines[5:]:
             line_list = line.strip().split('\t')
-            samp_values = np.array(line_list[9:])[samp_idx]  
+            samp_values = np.array(line_list[9:])[all_idx]  
             othr_values = line_list[:9]
             new_line = '\t'.join(othr_values) + '\t' + '\t'.join(samp_values) + '\n'
             f.write(new_line) 
+    print('{:>4} wrote ALL variants'.format(''))
 
     ## pdx snps 
+    pdx_vcf = '{}/chr{}.vcf'.format(pdx_dir, ch)
+    with open(pdx_vcf, 'r') as f: pdx_lines = f.readlines() 
+    print('{:>4} read PDX variants'.format(''))
+
     f_samp_pdx = '{}/chr{}.vcf'.format(samp_pdx_dir, ch) 
     with open(f_samp_pdx, 'w') as f: 
         f.write(header)
         f.write(new_cat_names) 
         for line in pdx_lines[5:]:
             line_list = line.strip().split('\t')
-            samp_values = np.array(line_list[9:])[samp_idx]  
+            samp_values = np.array(line_list[9:])[all_idx]  
             othr_values = line_list[:9]
             new_line = '\t'.join(othr_values) + '\t' + '\t'.join(samp_values) + '\n'
             f.write(new_line) 
+    print('{:>4} wrote PDX variants'.format(''))
 
+    tpass = pretty_time(time()-tstart)
+    print('{:>4} finished in {}'.format('', tpass))
